9b184f8f9aa949040e3ef435d5dfd0e60b9bce24
Batuhan Karaoglu <batuhan.karaoglu@ardictech.com>
2024-06-05 15:39:32 +0300

    fix deprecated members

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -14,30 +14,34 @@ class ThemeColors {
       brightness: Brightness.light,
       fontFamily: 'Poppins',
       primaryColor: primaryColor,
-      backgroundColor: lightBackgroundColor,
+      colorScheme: ColorScheme.light(
+        background: lightBackgroundColor,
+      ),
       scaffoldBackgroundColor: lightBackgroundColor,
       visualDensity: VisualDensity.adaptivePlatformDensity,
       appBarTheme: AppBarTheme(
         backgroundColor: lightBackgroundColor,
       ),
-      textTheme: TextTheme(button: TextStyle(color: lightTextColor)));
+      textTheme: TextTheme(labelLarge: TextStyle(color: lightTextColor)));
 
   static final darkTheme = ThemeData(
     brightness: Brightness.dark,
     fontFamily: 'Poppins',
     primaryColor: primaryColor,
-    backgroundColor: darkBackgroundColor,
+    colorScheme: ColorScheme.dark(
+      background: darkBackgroundColor,
+    ),
     scaffoldBackgroundColor: const Color(0xFF00040F),
     visualDensity: VisualDensity.adaptivePlatformDensity,
     textTheme: TextTheme(
-      button: TextStyle(color: darkTextColor),
+      labelLarge: TextStyle(color: darkTextColor),
     ),
     appBarTheme: AppBarTheme(
       backgroundColor: darkBackgroundColor,
     ),
   );
   static Brightness get currentSystemBrightness =>
-      SchedulerBinding.instance.window.platformBrightness;
+      SchedulerBinding.instance.platformDispatcher.platformBrightness;
 }
 
 extension ThemeExtras on ThemeData {
